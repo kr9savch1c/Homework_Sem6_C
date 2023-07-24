94e5140da304b6d26175a543da9e1b099fb23572
Danil <gadik2002@gmail.com>
2023-07-24 21:48:01 +0500

    Exercise 43 success

---
+++ Exercise 43/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите переменную b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+Console.Write($"\n Точка пересечения двух прямых: [{x},{y}]");
